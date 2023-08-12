17d05f8b3f6dedf963cef8eab41340016cbfa781
James Tersoo <gbaerenjames4god@gmail.com>
2023-08-11 23:55:41 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
  * main - 4-puts.c
  * description - Write a C program that prints exactly
  * Return: Always 0 (Success)
- */
+  */
 
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
